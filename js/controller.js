e2b42fbc3b25b7794bb09f13c701dd0727ef9604
Chrismarcel James <cmarcelj@gmail.com>
2018-06-28 10:18:44 +0100

    Create listCurrencies static method

--- js/controller.js
+++ js/controller.js
@@ -9,10 +9,31 @@ class CurrencyConverter {
     constructor(currencies, value) {
         this.currencies = currencies;
         this.value = value;
+
+        CurrencyConverter.convertCurrencies(currencies, value);
     }
 
     checkServiceWorker() {
         if (!navigator.serviceWorker) return;
         navigator.serviceWorker.register('./sw.js');
+    }
+
+    static listCurrencies(selectElems) {
+        const baseURL = 'https://free.currencyconverterapi.com/api/v5/currencies';
+        const option = [];
+        return fetch(baseURL)
+            .then(response => {
+                return response.json();
+            })
+            .then(allCurrencies => {
+                return selectElems.map((selectElems, index) => {
+                    Object.values(allCurrencies.results).map(currency => {
+                        option[index] = document.createElement('option');
+                        option[index].value = currency.id;
+                        option[index].textContent = `${currency.currencyName} (${currency.id})`;
+                        selectElems.appendChild(option[index]);
+                    });
+                });
+            });
     }
 }
